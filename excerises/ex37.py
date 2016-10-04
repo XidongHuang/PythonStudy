46a919dffe32d660e2ba699d3feca481a4080ab8
Gzink <gzink@live.cn>
2016-10-03 23:52:38 -0400

    Excercise 37 to 39, review, list and dictionariy(Map)

---
+++ excerises/ex37.py
@@ -0,0 +1,19 @@
+"""*************************************************************************
+    > File Name: ex37.py
+    > Author: XidongHuang (Tony)
+    > Mail: xidonghuang@gmail.com 
+    > Created Time: Mon  3 Oct 17:53:34 2016
+ ************************************************************************"""
+
+# -*- coding: utf-8 -*-
+
+str = 'Hello World'
+print(str)
+
+list = [4, 6, 'Ocean']
+print(list)
+
+float = 5.62
+print(float)
+
+print(float ** 2)
