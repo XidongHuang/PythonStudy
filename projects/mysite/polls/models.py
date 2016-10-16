a73ff25751dbf8e3881ad8510ed55236d900dd78
Gzink <gzink@live.cn>
2016-10-16 19:08:28 -0400

    Web generic views, and use unittest to test model and view

--- projects/mysite/polls/models.py
+++ projects/mysite/polls/models.py
@@ -15,7 +15,8 @@ class Question(models.Model):
 		return self.question_text
 
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 @python_2_unicode_compatible
 class Choice(models.Model):
@@ -25,3 +26,4 @@ class Choice(models.Model):
 
 	def __str__(self):
 		return self.choice_text
+
