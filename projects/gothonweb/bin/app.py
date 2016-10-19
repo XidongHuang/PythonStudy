2371125d408340f0b30060c79b4cccf4325a3937
Gzink <gzink@live.cn>
2016-10-19 17:00:29 -0400

    Finish LPTHW

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -9,19 +9,45 @@
 #!/usr/bin/env python3.5
 
 import web
+from gothonweb import map
 
 urls = (
-		'/', 'Index'
+		'/game', 'GameEngine',
+		'/', 'Index',
 		)
 
 app = web.application(urls, globals())
+
+if web.config.get('_session') is None:
+	store = web.session.DiskStore('sessions')
+	session = web.session.Session(app, store, initializer = {'room': None})
+	
+	web.config._session = session
+else:
+	session = web.config._session
+
 
 render = web.template.render("templates/")
 
 class Index(object):
 	def GET(self):
-		greeting = "Hello World"
-		return render.index(greeting = greeting)
+		session.room = map.START
+		web.seeother("/game")
+
+class GameEngine(object):
+	def GET(self):
+		if session.room:
+			return render.show_room(room=session.room)
+		else:
+			return render.you_died()
+
+def POST(self):
+	form = web.input(action=None)
+
+	if session.room and form.action:
+		session.room = session.room.go(form.action)
+	
+	web.seeother("/game")
 
 if __name__ == "__main__":
 	app.run()
